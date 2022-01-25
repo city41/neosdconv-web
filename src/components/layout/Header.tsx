1eb377b3de933cc285bb54861f7a7a254d979f1f
Matt Greer <matt.e.greer@gmail.com>
2022-01-25 13:20:04 -0500

    Header: remove logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './Root.module.css';
 
-import logoPng from './logo.png';
-
 type HeaderProps = {
 	className?: string;
 };
@@ -26,15 +24,6 @@ function Header({ className }: HeaderProps) {
 			<div className="mx-auto w-full max-w-7xl">
 				<div className="mx-auto mt-2  mb-4 flex h-32 w-full max-w-7xl flex-col flex-wrap items-start justify-between pl-20 sm:mt-0 sm:mb-0 sm:h-16 sm:flex-row sm:items-center sm:space-y-0 sm:space-x-8 sm:pl-8">
 					<div className="absolute top-1 left-1 flex flex-row items-center sm:static">
-						<a href="/" className={clsx(styles.logo, 'block lg:-ml-4')}>
-							<img
-								className="h-full w-full"
-								src={logoPng.src}
-								alt="smaghetti logo"
-								width={48}
-								height={48}
-							/>
-						</a>
 						<div className="text-focal hidden pl-4 text-center text-2xl font-light sm:block">
 							<a href="/">Rom to NeoSD</a>
 						</div>
